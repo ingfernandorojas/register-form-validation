97b1f8756bc4fa50c20fd10d835525bf099ba747
fernando <fernandorojasmosqueira@gmail.com>
2019-02-06 00:07:26 -0300

    Validacion de formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormvalidationComponent } from './formvalidation/formvalidation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormvalidationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
